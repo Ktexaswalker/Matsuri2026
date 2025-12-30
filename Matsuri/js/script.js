809fe7dec9a188101193d8e2f30e05f4f9097969
Ktexaswalker <hector.mc1988@gmail.com>
2025-12-30 20:05:40 +0100

    Idioma, medificacion de estilos, adaptacion estructura e imagenes

---
+++ Matsuri/js/script.js
@@ -0,0 +1,212 @@
+let logo = document.getElementsByClassName("navbar-toggler-icon")[0];
+if (logo) {
+    const borde = document.createElement("div");
+    borde.style.position = `relative`;
+    const fondo = document.createElement("div");
+    fondo.setAttribute("class", "fondo");
+    logo.parentNode.insertBefore(fondo, logo);
+    fondo.style.padding = `9px`;
+    fondo.appendChild(logo);
+    let angle = 0;
+    let porcentaje = 0;
+    let mas = 0;
+    let add = 0;
+    setInterval(() => {
+        if (angle >= 360) angle = 0;
+        if (angle % 90) {
+            porcentaje += 1;
+        }
+        if (porcentaje >= 200) {
+            porcentaje = 0; 
+            mas += add;
+            if (angle < 36)  {
+                add = -1;
+                if (mas == 0) {
+                    add = +1;
+                }
+            }
+            if (add < 0 && mas < 0) {
+                add += 1;
+                mas = 0;
+            }
+        }
+        angle += 1;
+        fondo.style.background = `conic-gradient(from ${angle}deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.1)), url("./img/logo/acj\ logo red.png")`;
+        fondo.style.backgroundSize = 'cover';
+        fondo.style.borderRadius = `50%`;
+        fondo.style.backgroundRepeat = 'no-repeat';
+        fondo.style.backgroundPosition = 'center';
+        fondo.style.backgroundSize = `${Math.round(mas)*Math.round(mas)}%`;
+    }, 20);
+}
+
+// Carousel
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 16,
+    loop: false,
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+        slidesPerView: 1,   // móvil
+        },
+        576: {
+        slidesPerView: 2,   // tablet
+        },
+        768: {
+        slidesPerView: 3,   // desktop
+        },
+    },
+});
+
+// IDIOMA
+const langBtns = document.querySelectorAll(".langBtn");
+const items = document.querySelectorAll(".idioma");
+const idiomas = document.querySelectorAll(".dropdown-item.idioma");
+
+const browserLangMap = {
+        es: "Español",
+        ca: "Català",
+        en: "English",
+        ja: "日本語"
+};
+
+const savedLang = localStorage.getItem("lang");
+const browserLang = (navigator.language || navigator.userLanguage).slice(0, 2);
+const currentLang = savedLang || browserLangMap[browserLang] || "English";
+
+langBtns.forEach(btn => {
+    btn.textContent = currentLang;
+})
+
+// Cambio manual
+items.forEach(item => {
+    item.addEventListener("click", (e) => {
+        e.preventDefault();
+        const selectedLang = item.textContent.trim();
+        translatePage(selectedLang);
+        langBtns.forEach(btn => {
+            btn.textContent = selectedLang;
+        });
+        localStorage.setItem("lang", selectedLang);
+        idiomas.forEach(item => {
+        if (item.textContent.trim() === selectedLang) {
+            item.classList.add("d-none");
+        } else {
+            item.classList.remove("d-none");
+        }
+    });
+    });
+});
+// oculta el idioma guardado en la lista de idiomas y muestra el resto
+const langBtn = [...langBtns].map(
+    btn => btn.textContent.trim()
+);
+items.forEach(i => {
+    const item = i.textContent.trim();
+    if (langBtn.includes(item)) {
+        i.classList.add("d-none");
+    } else {
+        i.classList.remove("d-none");
+    }
+});
+    
+function applyTranslation() {
+    translatePage(savedLang);
+}
+
+window.addEventListener('load', applyTranslation);
+window.addEventListener('idioma', applyTranslation);
+
+
+// Optimización de imagenes
+function convertToWebP(imgElement) {
+  return new Promise((resolve) => {
+    const jpgUrl = imgElement.dataset.src;
+    const img = new Image();
+    img.src = jpgUrl;
+
+    img.onload = () => {
+        const canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+
+        const webpDataUrl = canvas.toDataURL('image/webp', 0.8);
+        imgElement.src = webpDataUrl;
+        resolve();
+    };
+    
+    img.onerror = () => {
+        console.warn(`No se pudo cargar la imagen: ${jpgUrl}`);
+        imgElement.src = jpgUrl;
+        resolve();
+    };
+  });
+}
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+        convertToWebP(entry.target);
+        obs.unobserve(entry.target);
+        }
+    });
+    }, { rootMargin: '50px' });
+} else {
+    document.querySelectorAll('.optimizable').forEach(img => {
+        convertToWebP(img);
+    });
+}
+
+// LUPA
+const lupa = document.querySelectorAll(".lupa").forEach(lupa => {
+    const img = lupa.querySelector("img");
+    let index = 1;
+    lupa.addEventListener("contextmenu", e => e.preventDefault());
+    lupa.addEventListener("pointerdown", e => {
+        if (e.button === 0) {
+            index++;
+            console.log("izquierdo");
+        }
+        if (e.button === 1) {
+            console.log("central");
+        }
+        if (e.button === 2) {
+            console.log("derecho");
+            index--;
+        }
+    });
+
+    lupa.addEventListener("pointermove", e => {
+        const rect = lupa.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const px = (x / rect.width) * 100;
+        const py = (y / rect.height) * 100;
+
+        img.style.transformOrigin = `${px}% ${py}%`;
+        img.style.transform = `scale(${index})`;
+    });
+
+    lupa.addEventListener("pointerup", () => {
+        img.style.transform = `scale(${index})`;
+        img.style.transformOrigin = "center";
+    });
+
+    lupa.addEventListener("pointerleave", () => {
+        img.style.transformOrigin = "center";
+        img.style.transform = `scale(1)`;
+    });
+})
---
+++ Matsuri/js/translate.js
@@ -0,0 +1,143 @@
+const translations = {
+    Español: {
+        "MATSURI FESTIVAL BARCELONA 2026":"MATSURI FESTIVAL BARCELONA 2026",
+        "16 and 17 of may - At moll de la fusta":"16 y 17 de maig - En el moll de la fusta",
+        "Welcome": "Bienvenido",
+        "Buy your tickets now": "Compra tus entradas ahora",
+        "TICKETS": "ENTRADAS",
+        "Activities": "Actividades",
+        "Program": "Programa",
+        "Performers": "Artistas",
+        "Kids": "Niños",
+        "Booths": "Estands",
+        "Food": "Comida",
+        "About":"Acerca de",
+        "Support us":"Apóyanos",
+        "Contact":"Contacto",
+        "Sponsor us":"Patrocinador",
+        "Crowdfund us":"Micromecenazgo",
+        "Volunteer":"Voluntariado",
+        "What is the Matsuri BCN?":"",
+        "Matsuri BCN is a Japanese cultural festival here in Barcelona where people can come and enjoy live performance, authentic food and activities just like in Japan!":"Matsuri BCN es un festival cultural japonés aquí en Barcelona donde la gente puede venir y disfrutar de actuaciones en vivo, comida auténtica y actividades como en Japón.",
+        "Information":"Información",
+        "PROGRAM":"PROGRAMA",
+        "PERFORMERS":"INTERPRETES",
+        "KIDS":"NIÑOS",
+        "BOOTHS":"ESTANDS",
+        "FOOD":"COMIDA",
+        "Where is the Matsuri?":"¿Dónde está el Matsuri?",
+        "Moll de la fusta":"Muelle de la Madera - Moll de la Fusta",
+        "Ronda Litoral, 1740, Ciutat Vella, 08039 Barcelona, 08039":"Ronda Litoral, 1740, Ciutat Vella, 08039 Barcelona",
+        "Want to help the Matsuri?":"¿Quieres ayudar al Matsuri?",
+        "Become a volunteer":"Hazte voluntario",
+        "FAQ's":"Preguntas frequentes",
+        "Contact":"Contacto",
+        "Cookie policy":"Política de cookies",
+        "Privacy policy":"Política de privacidad",
+    },
+    Català: {
+        "MATSURI FESTIVAL BARCELONA 2026":"MATSURI FESTIVAL BARCELONA 2026",
+        "16 and 17 of may - At moll de la fusta":"16 y 17 de mayo - En el moll de la fusta",
+        "Welcome": "Benvingut",
+        "Buy your tickets now": "Compra les teves entrades ara",
+        "TICKETS": "ENTRADES",
+        "Activities": "Activitats",
+        "Program": "Programa",
+        "Performers": "Artistas",
+        "Kids": "Nens",
+        "Booths": "Estands",
+        "Food": "Menjar",
+        "About":"Referent a",
+        "Support us":"Dóna'ns suport",
+        "Contact":"Contacte",
+        "Sponsor us":"Patrocinador",
+        "Crowdfund us":"Micromecenatge",
+        "Volunteer":"Voluntariat",
+        "What is the Matsuri BCN?":"On és el Matsuri?",
+        "Matsuri BCN is a Japanese cultural festival here in Barcelona where people can come and enjoy live performance, authentic food and activities just like in Japan!":"Matsuri BCN és un festival cultural japonès aquí a Barcelona on la gent pot venir i gaudir d'actuacions en directe, menjar autèntic i activitats com al Japó!",
+        "Information":"Informació",
+        "PROGRAM":"PROGRAMA",
+        "PERFORMERS":"INTERPRETS",
+        "KIDS":"NENS",
+        "BOOTHS":"ESTANDS",
+        "FOOD":"MENJAR",
+        "Where is the Matsuri?":"On és el Matsuri?",
+        "Moll de la fusta":"Moll de la fusta",
+        "Ronda Litoral, 1740, Ciutat Vella, 08039 Barcelona":"Ronda Litoral, 1740, Ciutat Vella, 08039 Barcelona",
+        "Want to help the Matsuri?":"Vols ajudar en el Matsuri?",
+        "Become a volunteer":"Fes-te voluntari",
+        "FAQ's":"Preguntes freqüents",
+        "Contact":"Contacte",
+        "Cookie policy":"Política de cookies",
+        "Privacy policy":"Política de privacitat",
+    },
+    日本語: {
+        "MATSURI FESTIVAL BARCELONA 2026":"バルセロナ祭り2026",
+        "16 and 17 of may - At moll de la fusta":"5月16日と17日 - モール・デ・ラ・フスタにて",
+        "Welcome": "ようこそ",
+        "Buy your tickets now": "チケットを購入してください",
+        "TICKETS": "チケット",
+        "Activities": "活動",
+        "Program": "プログラム",
+        "Performers": "出演者",
+        "Kids": "子供たち",
+        "Booths":"ブース",
+        "Food": "食べ物",
+        "About":"について",
+        "Support us":"私たちをサポートしてください",
+        "Contact":"接触",
+        "Sponsor us":"スポンサーになる",
+        "Crowdfund us":"クラウドファンディングで支援を",
+        "Volunteer":"ボランティア",
+        "What is the Matsuri BCN?":"Matsuri BCNとは何ですか？",
+        "Matsuri BCN is a Japanese cultural festival here in Barcelona where people can come and enjoy live performance, authentic food and activities just like in Japan!":"Matsuri BCN はバルセロナで開催される日本文化のお祭りで、日本と同じようにライブパフォーマンス、本格的な料理、アクティビティを楽しむことができます。",
+        "Information":"情報",
+        "PROGRAM":"プログラム",
+        "PERFORMERS":"出演者",
+        "KIDS":"キッズ",
+        "BOOTHS":"ブース",
+        "FOOD":"食べ物",
+        "Where is the Matsuri?":"祭りはどこですか？",
+        "Moll de la fusta":"木製の桟橋 - もゆでラフスタ - Moll de la fusta",
+        "Ronda Litoral, 1740, Ciutat Vella, 08039 Barcelona":"ロンダ リトラル、1740、シウタット ベーリャ、08039 バルセロナ",
+        "Want to help the Matsuri?":"祭りを手伝ってみませんか？",
+        "Become a volunteer":"ボランティアになる",
+        "FAQ's":"よくある質問",
+        "Contact":"接触",
+        "Cookie policy":"クッキーポリシー",
+        "Privacy policy":"プライバシーポリシー",
+    }
+};
+
+const originalTextNodes = [];
+
+const walker = document.createTreeWalker(
+  document.body,
+  NodeFilter.SHOW_TEXT,
+  null
+);
+
+let node;
+while ((node = walker.nextNode())) {
+  const text = node.nodeValue.replace(/\s+/g, ' ').trim();
+  if (text) {
+    originalTextNodes.push({
+      node,
+      text
+    });
+  }
+}
+
+function translatePage(lang) {
+  const dict = translations[lang];
+
+  originalTextNodes.forEach(({ node, text }) => {
+    if (lang === 'English') {
+      node.nodeValue = text;
+      return;
+    }
+    if (dict[text]) {
+      node.nodeValue = dict[text];
+    }
+  });
+}
