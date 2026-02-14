6eaf4f3076ce7d3c97ef6eb1e30f34114bb1dec5
Ktexaswalker <hector.mc1988@gmail.com>
2026-02-14 08:52:58 +0100

    corrección japonés a romaji

--- Matsuri/games/jsGames/frameWords.js
+++ Matsuri/games/jsGames/frameWords.js
@@ -3,18 +3,18 @@ words = {
         English: ["dog", "log", "frog"],
         Español: ["mago", "lago", "mango"],
         Català: ["poma", "coma", "copa"],
-        日本語: ["ねこ", "ねず", "ねま"],
+        日本語: ["neko", "nezu", "nema"],
     },
     medium : {
         English: ["deer", "bear", "fear"],
         Español: ["suma", "puma", "pluma"],
         Català: ["peix", "peça", "premsa"],
-        日本語: ["はな", "はね", "はま"],
+        日本語: ["hana", "hane", "hama"],
     },
     hard : {
         English: ["map", "rat", "bat", "cut"],
         Español: ["arte", "marte", "muerte", "fuerte"],
         Català: ["pica", "vinya", "mina", "pila"],
-        日本語: ["さけ", "さか", "さる", "さっか"],
+        日本語: ["sake", "saka", "saru", "sakka"],
     }
 }
